72689d12093ba256da300ae104e7800ee6dbf034
Ričardas <ricardvers@gmail.com>
2020-08-28 17:22:51 +0300

    first commit

---
+++ app/src/main/java/com/richve/Dinnerdecider/ListActivity.kt
@@ -0,0 +1,56 @@
+package com.richve.Dinnerdecider
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_list.*
+import kotlin.system.exitProcess
+
+class ListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val myList =
+            intent.getSerializableExtra("Listas") as List<String?>
+        val arrayAdapter: ArrayAdapter<*>
+        var myListView = findViewById<ListView>(R.id.listView)
+        arrayAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, myList
+
+        )
+        myListView.adapter = arrayAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+
+            R.id.nav_about -> onBackPressed()
+            R.id.nav_quit -> {
+                this@ListActivity.finish()
+
+                exitProcess(0)
+            }
+        }
+        exitProcess(0)
+    }
+}
---
+++ app/src/main/java/com/richve/Dinnerdecider/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.richve.Dinnerdecider
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+import kotlin.system.exitProcess
+
+
+class MainActivity : AppCompatActivity() {
+
+    public var foodList = arrayListOf("Chinese", "McDonald's", "Pizza", "Sushi")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        btn_search.setOnClickListener {
+            onSearchClick()
+        }
+
+        btn_decide.setOnClickListener {
+            val random = Random
+            var randomFood = random.nextInt(foodList.count())
+            val food = foodList[randomFood]
+            TextView.text =  foodList[randomFood].toString()
+            btn_search.visibility = View.VISIBLE
+
+        }
+            btn_add.setOnClickListener {
+
+                if (et_add.text.toString().isNotBlank()) {
+                    val newFood = et_add.text.toString()
+                    foodList.add(newFood)
+                    Toast.makeText(this, "$newFood was added to your list!", Toast.LENGTH_SHORT)
+                        .show()
+                    et_add.text.clear()
+
+
+                } else Toast.makeText(this, "Please write something first", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            btn_list.setOnClickListener {
+                val intent = Intent(this, ListActivity::class.java).apply {
+                    putExtra("Listas", foodList);
+                    startActivity(this)
+                }
+
+            }
+        }
+
+
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+            menuInflater.inflate(R.menu.menu, menu)
+            return true
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+            when (item.itemId) {
+
+                R.id.nav_about -> Toast.makeText(this, "About", Toast.LENGTH_SHORT).show()
+                R.id.nav_quit -> {
+                    this@MainActivity.finish()
+                    exitProcess(0)
+                }
+            }
+
+            return super.onOptionsItemSelected(item)
+        }
+
+         private fun onSearchClick() {
+            try {
+                val term: String = TextView.text.toString()
+                val gmmIntentUri =
+                    Uri.parse("geo:0,0?q=" + Uri.encode(term + " restaurant"))
+                val searchIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                searchIntent.setPackage("com.google.android.apps.maps")
+                startActivity(searchIntent)
+            } catch (e: Exception) {
+                Toast.makeText(this, "Ooops, something went wrong...", Toast.LENGTH_SHORT).show()
+            }
+        }
+        override fun onPause() {
+        super.onPause()  // Always call the superclass method first
+
+    }
+}
