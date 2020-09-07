e301d56f86ca5bf3cd7a31744080756710e6f34c
Ričardas <ricardvers@gmail.com>
2020-09-07 12:07:32 +0300

    s

--- app/src/main/java/com/richve/Dinnerdecider/ListActivity.kt
+++ app/src/main/java/com/richve/Dinnerdecider/ListActivity.kt
@@ -1,69 +1,91 @@
 package com.richve.Dinnerdecider
 
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.CheckedTextView
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.system.exitProcess
+import androidx.preference.PreferenceManager
+import com.richve.Dinnerdecider.ui.getDefaults
+import com.richve.Dinnerdecider.ui.setDefaults
+import kotlinx.android.synthetic.main.activity_list.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class ListActivity : AppCompatActivity() {
+
+    var selected = arrayListOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        var myList =
-            intent.getSerializableExtra("Listas") as List<String?>
-
+        val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        var myList = ArrayList(getDefaults("maistas", this)?.split(","))
         val arrayAdapter: ArrayAdapter<*>
         var myListView = findViewById<ListView>(R.id.listView)
+
 
         arrayAdapter = ArrayAdapter(
             this,
             android.R.layout.simple_list_item_multiple_choice, myList
+
         )
         myListView?.adapter = arrayAdapter
-
-        myListView.setOnItemClickListener { AdapterView, view, i, l ->
-
-            (view as CheckedTextView).toggle()
-
-            if (!view.isChecked)
-            {
+        myListView?.setOnItemClickListener { _, view, i, _ ->
+            (view as Checkable).toggle()
+            if (!view.isChecked) {
                 view.setBackgroundColor(Color.WHITE)
+                selected.remove(i)
+            } else {
+                view.setBackgroundColor(Color.LTGRAY)
+                selected.add(i)
             }
-            else
-                view.setBackgroundColor(Color.BLUE)
-            Toast.makeText(this, "${view.isChecked}", Toast.LENGTH_SHORT).show()
-        }
 
+        }
+        btn_delete.setOnClickListener {
+            Collections.sort(selected, Collections.reverseOrder())
+            for (i in selected) myList.removeAt(i)
+            setDefaults("maistas", myList.joinToString(","), this)
+            arrayAdapter.notifyDataSetChanged()
+            selected.clear()
+
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
-        menuInflater.inflate(R.menu.menu, menu)
+    override fun onCreateOptionsMenu(list_menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.list_menu, list_menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         when (item.itemId) {
+            R.id.action_one -> {
 
-            R.id.nav_about -> onBackPressed()
+            }
+            R.id.nav_about -> {
+                Toast.makeText(applicationContext, "fdfd", Toast.LENGTH_SHORT).show()
+            }
             R.id.nav_quit -> {
-
-                exitProcess(0)
+                Toast.makeText(applicationContext, "fdfd", Toast.LENGTH_SHORT).show()
             }
         }
         return (false)
+    }
+
+    fun setChecked(view : View, checked: Boolean = false   ) {
+        (view as Checkable).isChecked = checked
+    }
+
+    fun delete ( listView: ListView, view: View) {
+
+            (view as Checkable).isChecked = false
     }
 
 }
--- app/src/main/java/com/richve/Dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/richve/Dinnerdecider/MainActivity.kt
@@ -1,76 +1,70 @@
 package com.richve.Dinnerdecider
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
-import android.system.Os.remove
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.richve.Dinnerdecider.ui.getDefaults
+import com.richve.Dinnerdecider.ui.setDefaults
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONStringer
 import kotlin.random.Random
 import kotlin.system.exitProcess
 
 
+
 class MainActivity : AppCompatActivity() {
 
-    public var foodList = arrayListOf("Chinese", "McDonald's", "Pizza", "Sushi")
-    public var defaultList = foodList.joinToString(",")
-    private val PREF_NAME = "maisto-listas"
-    private val PREF_KEY = "maistas"
+    var foodList = arrayListOf("Chinese", "McDonald's", "Pizza", "Sushi")
+    var defaultList = foodList.joinToString(",")
+    val PREF_NAME = "maisto-listas"
+    val PREF_KEY = "maistas"
     private val PRIVATE_MODE = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val sharedPref: SharedPreferences = getSharedPreferences(PREF_NAME, PRIVATE_MODE);
-        foodList = ArrayList(sharedPref.getString(PREF_KEY, defaultList)?.split(","))
+
+        foodList = ArrayList(getDefaults(PREF_KEY, this)?.split(","))
         setContentView(R.layout.activity_main)
 
 
         btn_search.setOnClickListener {
             onSearchClick()
+            true
         }
 
         btn_decide.setOnClickListener {
             val random = Random
             var randomFood = random.nextInt(foodList.count())
             val food = foodList[randomFood]
-            TextView.text =  foodList[randomFood].toString()
+            tv_result.text =  foodList[randomFood].toString()
             btn_search.visibility = View.VISIBLE
+            btn_search?.setText("find ${tv_result.text} restaurants")
 
 
         }
             btn_add.setOnClickListener {
+
                 if (et_add.text.toString().isNotBlank()) {
                     val newFood = et_add.text.toString()
                     foodList.add(newFood)
-
-                    with (sharedPref.edit()) {
-                        putString(PREF_KEY, foodList.joinToString(","))
-                        commit()
-                    }
-
+                    setDefaults(PREF_KEY, foodList.joinToString(","), this)
                     Toast.makeText(this, "$newFood was added to your list!", Toast.LENGTH_SHORT)
                         .show()
                     et_add.text.clear()
-
-
 
                 } else Toast.makeText(this, "Please write something first", Toast.LENGTH_SHORT)
                     .show()
             }
 
             btn_list.setOnClickListener {
-                val intent = Intent(this, ListActivity::class.java).apply {
-                    putExtra("Listas", foodList);
+                    val intent = Intent(applicationContext, ListActivity::class.java).apply {
+                    //putExtra("Listas", foodList);
                     startActivity(this)
                 }
 
@@ -78,7 +72,7 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        override fun onCreateOptionsMenu(menu: Menu): Boolean {
 
             menuInflater.inflate(R.menu.menu, menu)
             return true
@@ -100,7 +94,7 @@ class MainActivity : AppCompatActivity() {
 
          private fun onSearchClick() {
             try {
-                val term: String = TextView.text.toString()
+                val term: String = tv_result.text.toString()
                 val gmmIntentUri =
                     Uri.parse("geo:0,0?q=" + Uri.encode(term + " restaurant"))
                 val searchIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
